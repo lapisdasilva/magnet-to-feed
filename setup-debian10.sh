9085d783d52fc5eaa723a62904be62099c7cf996
LapisDaSilva <66370371+lapisdasilva@users.noreply.github.com>
2023-11-07 22:09:56 -0300

    Update setup-debian10.sh

--- setup-debian10.sh
+++ setup-debian10.sh
@@ -7,14 +7,14 @@ apt upgrade -y
 # Instalar o Nginx
 apt-get install nginx -y
 
-# Instalar o PHP e os módulos necessários para o Nginx
+# Instalar o PHP e os mÃ³dulos necessÃ¡rios para o Nginx
 apt-get install php-fpm -y
 
-# Reinicie o Nginx e o PHP-FPM para aplicar as alterações
+# Reinicie o Nginx e o PHP-FPM para aplicar as alteraÃ§Ãµes
 systemctl restart nginx
 systemctl restart php7.3-fpm
 
-# Instale as dependências do sistema para o feed
+# Instale as dependÃªncias do sistema para o feed
 apt-get install -y python3
 apt-get install -y python3-pip
 apt-get install -y wget
@@ -27,5 +27,11 @@ pip3 install qbittorrent-api
 
 mkdir /var/www/html/rss
 cp criar_lista.py rotina_rss.sh index.html /var/www/html/rss/
+
+# Defina a linha do Crontab que deseja adicionar
+cron_line="*/5 * * * * cd /var/www/html/rss/ && bash ./rotina_rss.sh"
+
+# Adicione a linha ao Crontab
+(crontab -l ; echo "$cron_line") | crontab -
 
 echo "Script executado com sucesso!"
