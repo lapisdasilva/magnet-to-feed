ab1784dc287fd821491dafcf1f21885c6136a2b6
LapisDaSilva <66370371+lapisdasilva@users.noreply.github.com>
2023-11-07 22:03:47 -0300

    Update rotina_rss.sh
    atualiação que junta todos os processos em 1 so arquivo

--- rotina_rss.sh
+++ rotina_rss.sh
@@ -3,10 +3,10 @@
 # Defina a URL do feed RSS
 rss_url="http://fitgirl-repacks.site/feed/"
 
-# Nome do arquivo de saída
+# Nome do arquivo de saÃ­da
 output_file="fit.html"
 
-# Use o wget para baixar o feed RSS e salve-o no arquivo de saída
+# Use o wget para baixar o feed RSS e salve-o no arquivo de saÃ­da
 wget -O "$output_file" "$rss_url"
 
 # Verifica se o download foi bem-sucedido
@@ -16,15 +16,41 @@ else
   echo "Ocorreu um erro ao baixar o feed RSS."
 fi
 
-python3 criar_lista.py
+python3 <<EOF
+import re
+
+# Abre o arquivo fit.html e lÃª seu conteÃºdo
+with open('fit.html', 'r', encoding='utf-8') as file:
+    html_content = file.read()
+
+# Usa uma expressÃ£o regular para encontrar os trechos que comeÃ§am com "[<a href=" e terminam com ">magnet</a>]"
+magnet_links = re.findall(r'\[<a href="(.*?)">magnet</a>]', html_content)
+
+# Cria um arquivo de saÃ­da para gravar os magnet links no formato desejado
+with open('magnet_links.txt', 'w', encoding='utf-8') as output_file:
+    if magnet_links:
+        for link in magnet_links:
+            # Extrai o nome do magnet da tag "dn" no link do magnet
+            name_match = re.search(r'dn=([^&#]+)', link)
+            if name_match:
+                name = name_match.group(1)
+                # Remove caracteres especiais, mantendo apenas os "+"
+                name = re.sub(r'[^\w+]', '', name)
+                # Limita o nome a no mÃ¡ximo 50 caracteres
+                name = name[:50]
+                output_file.write(f"{name}@{link}\n")  # Use o separador "@" aqui
+        print("Magnet links com nomes limitados a 50 caracteres extraÃ­dos com sucesso e salvos em magnet_links.txt")
+    else:
+        print("Nenhum link de magnet encontrado no arquivo.")
+EOF
 
 # Caminho para o arquivo magnet_links.txt
 ARQUIVO_TXT="magnet_links.txt"
 
-# Caminho para o arquivo XML de saída
+# Caminho para o arquivo XML de saÃ­da
 ARQUIVO_XML="feed.xml"
 
-# Cabeçalho do arquivo XML
+# CabeÃ§alho do arquivo XML
 cat <<EOF > "$ARQUIVO_XML"
 <?xml version="1.0" encoding="UTF-8"?>
 <rss version="2.0"
@@ -50,7 +76,7 @@ while IFS='@' read -r nome magnet; do
   echo "    </item>" >> "$ARQUIVO_XML"
 done < "$ARQUIVO_TXT"
 
-# Rodapé do arquivo XML
+# RodapÃ© do arquivo XML
 cat <<EOF >> "$ARQUIVO_XML"
   </channel>
 </rss>
