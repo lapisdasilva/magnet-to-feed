fcf4291c06421136f907e19eccaa751b9df11475
LapisDaSilva <66370371+lapisdasilva@users.noreply.github.com>
2023-11-07 21:11:13 -0300

    setup-debian10.sh

--- setup-debian10.sh
+++ setup-debian10.sh
@@ -1,30 +1,31 @@
 #!/bin/bash
 
 # Atualize o sistema
-apt update;
-apt upgrade -y;
+apt update
+apt upgrade -y
 
-# Instalar o Apache
-apt-get install apache2 -y;
+# Instalar o Nginx
+apt-get install nginx -y
 
-# Instalar o PHP e os módulos necessários
-apt-get install php libapache2-mod-php -y;
+# Instalar o PHP e os módulos necessários para o Nginx
+apt-get install php-fpm -y
 
-# Reiniciar o Apache para aplicar as alterações
-systemctl restart apache2;
+# Reinicie o Nginx e o PHP-FPM para aplicar as alterações
+systemctl restart nginx
+systemctl restart php7.3-fpm
 
 # Instale as dependências do sistema para o feed
-apt-get install -y python3;
-apt-get install -y python3-pip;
-apt-get install -y wget;
+apt-get install -y python3
+apt-get install -y python3-pip
+apt-get install -y wget
 
 # Certifique-se de usar o pip3 para instalar beautifulsoup4
-pip3 install beautifulsoup4;
+pip3 install beautifulsoup4
 
 # Instale qbittorrent-api usando pip3 para Python 3
-pip3 install qbittorrent-api;
+pip3 install qbittorrent-api
 
-mkdir /var/www/rss/;
+mkdir /var/www/rss
 cp criar_lista.py rotina_rss.sh /var/www/rss/
 
 echo "Script executado com sucesso!"
